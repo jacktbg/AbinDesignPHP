b17ce8b551d44b0419da8c8a1be2d975a6a3197e
jacktbg <jack.bustinza@tecsup.edu.pe>
2022-06-14 17:42:39 -0500

    index and composer

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="refresh" content="0; URL=./Views/Pages/Inicio.php" />
+  <title>Abin Design</title>
+</head>
+
+<body></body>
